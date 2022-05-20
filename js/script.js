3fac744b148d379772c0fa79f66a41b95f1982ed
Aarya <amathrej@asu.edu>
2022-05-20 03:49:00 -0700

    Add scientificToDecimal

--- js/script.js
+++ js/script.js
@@ -84,8 +84,44 @@ function whichFlag(){
 }
 
 function roundResult(num) {
-    return Math.round(number * 1000) / 1000;
+    return Math.round(num * 1000) / 1000;
 }
+
+let scientificToDecimal = function (num) {
+    let nsign = Math.sign(num);
+    //remove the sign
+    num = Math.abs(num);
+    //if the number is in scientific notation remove it
+    if (/\d+\.?\d*e[\+\-]*\d+/i.test(num)) {
+        let zero = '0',
+                parts = String(num).toLowerCase().split('e'), //split into coeff and exponent
+                e = parts.pop(), //store the exponential part
+                l = Math.abs(e), //get the number of zeros
+                sign = e / l,
+                coeff_array = parts[0].split('.');
+        if (sign === -1) {
+            l = l - coeff_array[0].length;
+            if (l < 0) {
+              num = coeff_array[0].slice(0, l) + '.' + coeff_array[0].slice(l) + (coeff_array.length === 2 ? coeff_array[1] : '');
+            } 
+            else {
+              num = zero + '.' + new Array(l + 1).join(zero) + coeff_array.join('');
+            }
+        } 
+        else {
+            let dec = coeff_array[1];
+            if (dec)
+                l = l - dec.length;
+            if (l < 0) {
+              num = coeff_array[0] + dec.slice(0, l) + '.' + dec.slice(l);
+            } else {
+              num = coeff_array.join('') + new Array(l + 1).join(zero);
+            }
+        }
+    }
+
+    return nsign < 0 ? '-'+num : num;
+};
 
 function equalFunction() {
     if(flagCheck()) return;
@@ -96,22 +132,22 @@ function equalFunction() {
 
 if(whichFlag() == '+')
 {
-    output = parseInt(output[0]) + parseInt(output[1]);
+    output = scientificToDecimal(output[0]) + scientificToDecimal(output[1]);
 }
 
  if(whichFlag() == '-')
 {
-    output = parseInt(output[0]) - parseInt(output[1]);
+    output = scientificToDecimal(output[0]) - scientificToDecimal(output[1]);
 } 
 
 if(whichFlag() == '*')
 {
-    output = parseInt(output[0]) * parseInt(output[1]);
+    output = scientificToDecimal(output[0]) * scientificToDecimal(output[1]);
 }
 
 if(whichFlag() == '%')
 {
-    output = parseInt(output[0]) / parseInt(output[1]);
+    output = scientificToDecimal(output[0]) / scientificToDecimal(output[1]);
 }
 
     displayNumber = outputScreen.textContent = roundResult(output);
